7903d2e9b9e37af882ada0deea5d42ab979e9e5e
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-04-09 22:05:30 +0200

    outline for G

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
 const dogs = [
     {
@@ -28,8 +28,63 @@ const dogs = [
         breed: 'Corgi',
         age: 2
     }
-]
+] 
 
-app.listen(3000, () => {
-    console.log('Listening to port 3000...')
+// GET
+app.get('/dogs', (req, res) => {
+    res.send(dogs)
+})
+
+app.get('/dogs/:id', (req, res) => {
+    const dog = dogs.find( d => d.id === parseInt(req.params.id))
+
+    if(!dog){
+        return res.status(404).send('Cannot find dog with this ID')
+    }
+    res.send(dog)
+})
+
+//POST
+app.post('/dogs', (req, res) => {
+    const dog = {
+        id: dogs.length + 1,
+        name: req.body.name,
+        breed: req.body.breed,
+        age: req.body.age
+    }
+
+    dogs.push(dog)
+    res.send(dog)
+})
+
+//PUT
+app.put('/dogs/:id', (req, res) => {
+    const dog = dogs.find(d => d.id == parseInt(req.params.id))
+    if(!dog){
+        return res.status(404).send('Can not find dog with this ID')
+    }
+
+    dog.name = req.body.name
+    dog.breed = req.body.breed
+    dog.age = req.body.age
+    res.send(dog)
+})
+
+//DELETE
+app.delete('/dogs/:id', (req, res) => {
+    const dog = dogs.find( d => d.id === parseInt(req.params.id))
+    if(!dog){
+        return res.status(404).send('The specific doggo was not found')
+    }
+
+    const index = dogs.indexOf(dog)
+    dogs.splice(index, 1)
+    res.send(dog)
+})
+
+
+const port = 5000
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
 })
